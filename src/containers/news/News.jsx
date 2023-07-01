2e7918af642c8d757dbe63a3cc76cee75f83a27b
Dmitriy Sakaluk <78900270+owin66@users.noreply.github.com>
2023-07-01 18:55:44 +0300

    add

--- src/containers/news/News.jsx
+++ src/containers/news/News.jsx
@@ -13,7 +13,7 @@ class News extends Component {
     searchQuery: '',
     result: {},
   };
-//sssw
+//sssww
   componentDidMount() {
     const { searchQuery } = this.state;
     this.fetchData(searchQuery);
